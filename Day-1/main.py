d2431b7b2081e826471da8f2a3cd08e6fd6c3861
Asad Mahmood <asadmhmd1234@gmail.com>
2022-02-11 19:40:52 +0530

    add day 1 part two

--- Day-1/main.py
+++ Day-1/main.py
@@ -9,3 +9,13 @@ for i in range(1, len(lines)):
         result += 1
 
 print(result)
+result = 0
+
+window_sum = lines[0] + lines[1] + lines[2]
+for i in range(3, len(lines)):
+    if lines[i] + lines[i - 1] + lines[i - 2] > window_sum:
+        result += 1
+    window_sum += lines[i]
+    window_sum -= lines[i - 3]
+
+print(result)
